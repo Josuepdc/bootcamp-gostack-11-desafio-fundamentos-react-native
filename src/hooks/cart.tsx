8c7d1aabbab428b771662a794b82e7328c4a1b33
josuepdc <jospdc@gmail.com>
2020-09-22 21:19:40 -0300

    Recuperando carrinho salvo no storage

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,7 +30,13 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const productsJSON = await AsyncStorage.getItem(
+        '@GoMarketplace:cartProducts',
+      );
+
+      if (productsJSON) {
+        setProducts(JSON.parse(productsJSON));
+      }
     }
 
     loadProducts();
