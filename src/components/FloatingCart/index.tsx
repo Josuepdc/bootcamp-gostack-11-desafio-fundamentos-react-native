dddf355d9a07a0d9dcc49710c6c8bc0f3828d4a8
josuepdc <jospdc@gmail.com>
2020-09-22 21:16:55 -0300

    Exibindo valor total e quantidade dos itens no carrinho

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 
@@ -24,15 +24,20 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
-
-    return formatValue(0);
+    const totalPrice = products.reduce(
+      (previousValue, product) =>
+        previousValue + product.price * product.quantity,
+      0,
+    );
+    return formatValue(totalPrice);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const totalItems = products.reduce(
+      (previousValue, product) => previousValue + product.quantity,
+      0,
+    );
+    return totalItems;
   }, [products]);
 
   return (
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -47,15 +47,20 @@ const Cart: React.FC = () => {
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return formatValue(0);
+    const totalPrice = products.reduce(
+      (previousValue, product) =>
+        previousValue + product.price * product.quantity,
+      0,
+    );
+    return formatValue(totalPrice);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const totalItems = products.reduce(
+      (previousValue, product) => previousValue + product.quantity,
+      0,
+    );
+    return totalItems;
   }, [products]);
 
   return (
